18ed75aa88837a589902121ad2f7052bc2725330
watsokel <watsokel@onid.oregonstate.edu>
2015-12-18 15:47:10 -0800

    force location services

--- app/src/main/java/com/watsonlogic/malenah/malenah3/MainActivity.java
+++ app/src/main/java/com/watsonlogic/malenah/malenah3/MainActivity.java
@@ -1,24 +1,78 @@
 package com.watsonlogic.malenah.malenah3;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    final Context context = this;
+    private LocationManager locationManager;
+    private boolean GPSEnabled;
+    private boolean networkEnabled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Source: http://stackoverflow.com/questions/15997079/getlastknownlocation-always-return-null-after-i-re-install-the-apk-file-via-ecli
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        GPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER); //check GPS status
+        networkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER); // check network statu
+        Log.d("LOCATION (provider)", LocationManager.NETWORK_PROVIDER);
+        if (!GPSEnabled || !networkEnabled){
+            Log.d("LOCATION (GPS)", "disabled, ask user to enable!");
+            //show dialog to allow user to enable location settings
+            AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+            dialog.setTitle("GPS Disabled");
+            dialog.setMessage("Location services must be enabled. Enable now?").setCancelable(false);
+
+            dialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    startActivityForResult(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS), 0);
+                }
+            });
+
+            dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    //nothing to do
+                }
+            });
+            dialog.show();
+            return;
+            //TODO: disable login btn and disallow user to proceed
+            //TODO: alert user with toast or dialog, on user network/GPS enabled, call onCreate again
+        }
+        Log.d("LOCATION (network)", "enabled!");
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        if (checkLocationPermission()) {
+            Log.d("LOCATION (permission)", "user granted permission!");
+        } else {
+            Log.d("LOCATION (permission)", "permission denied!");
+        }
+
         /*TODO: Call function to launch service to fetch data (see getData()) by calling startService(FetchAllDataService)*/
 
+    }
 
+    public boolean checkLocationPermission() {
+        String permission = "android.permission.ACCESS_FINE_LOCATION";
+        int res = this.checkCallingOrSelfPermission(permission);
+        return (res == PackageManager.PERMISSION_GRANTED);
     }
 
     /*TODO: Broadcast receiver class to receive the data broadcasted from FetchAllDataService */
