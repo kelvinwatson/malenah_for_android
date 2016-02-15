fe59caffd8af058e37298857d2b8bfde555ba877
watsokel <watsokel@onid.oregonstate.edu>
2016-02-15 13:03:55 -0800

    removing api

--- malenah_api/start_mongodb.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-service mongod start
-systemctl status mongod
-tail -1 /var/log/mongodb/mongod.log
--- malenah_api/status_mongodb.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-systemctl status mongod
-echo '\n'
-tail -1 /var/log/mongodb/mongod.log
-
-#forever start /root/beerApi/index.js &
-#forever start /root/Website/index.js &
